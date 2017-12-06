7551f2a7c569bc85d18d0b113022a2255be772aa
Mario Idival <marioidival@gmail.com>
2017-12-06 14:54:39 -0300

    Fix >>

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ use std::io::prelude::*;
 use moeda::repl;
 
 fn main() {
-    print!(">>");
+    print!(">> ");
     let stdin = io::stdin();
     let mut repl = repl::Repl::new();
     while let Some(line) = stdin.lock().lines().next() {
         if let Ok(source_code) = line {
             println!("{}", repl.eval(source_code));
         }
-        print!(">>");
+        print!(">> ");
         io::stdout().flush().ok().expect(
             "Ops... Something went wrong. :(",
         );
